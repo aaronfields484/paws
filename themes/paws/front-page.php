6bc8d8813b470dab3771d1511ff1dd568f4a0ede
aaronfields484 <aaronfields484@gmail.com>
2022-04-17 14:52:31 -0700

    feat: about section col p&m

--- themes/paws/front-page.php
+++ themes/paws/front-page.php
@@ -11,12 +11,12 @@
     <section class="front-page__about pt-5 pb-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 col-md-6 d-flex align-self-center flex-wrap">
+                <div class="col-lg-6 col-md-6 d-flex align-self-center flex-wrap pe-5 mt-2 mb-2">
                     <h2>About</h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis recusandae iure nihil dicta minus eligendi dolore inventore quisquam, animi unde magni numquam voluptatem perferendis laborum.</p>
                     <button class="btn button-secondary text-light">Learn More</button>
                 </div>
-                <div class="col-lg-6 col-md-6">
+                <div class="col-lg-6 col-md-6 mt-2 mb-2">
                     <img src="<?php echo get_template_directory_uri() ?>/img/about-placeholder.jpg" class="img-fluid rounded" alt="about-image">
                 </div>
             </div>
