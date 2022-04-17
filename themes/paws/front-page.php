69020f88b7d0ccd160e480fd2072a0af64c162ee
aaronfields484 <aaronfields484@gmail.com>
2022-04-17 13:43:11 -0700

    feat: main banner

--- themes/paws/front-page.php
+++ themes/paws/front-page.php
@@ -1,5 +1,11 @@
 <?php get_header() ?>
 
-
+    <header class="main-banner">
+        <div class="main-banner__content container text-light">
+            <h5 class="main-banner__subtitle">Lorem, ipsum dolor.</h6>
+            <h1 class="main-banner__title">Lorem ipsum dolor sit amet.</h1>
+            <h3 class="main-banner__desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, fugit!</h3>
+        </div>
+    </header>
 
 <?php get_footer() ?>
