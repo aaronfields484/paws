98b180769eab1c559eddb0e6910ec5db67194e1a
aaronfields484 <aaronfields484@gmail.com>
2022-05-11 10:16:44 -0700

    feat: layout change for blog pages

--- themes/paws/single.php
+++ themes/paws/single.php
@@ -1,44 +1,36 @@
 <?php
 get_header();
 ?>
-
-<section class="secondary-banner bg-dark p-5 mb-5">
+<?php if(have_posts()): the_post(); ?>
+<section style="<?php echo has_post_thumbnail() ? 'background-image: url('.get_the_post_thumbnail_url().');' : null ?>" class="secondary-banner bg-dark p-5 mb-5">
     <div class="container">
         <div class="secondary-banner__content text-light">
             <h1 class="text-wrap"><?php echo get_the_title(); ?></h1>
+            <p> 
+                <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?> 
+            </p>
+            <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
         </div>
     </div>
 </section>
 
 <section class="content-holder">
+    <div class="container">
+    <article class="post py-5">
 
-<article class="post">
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <div class= "article"> 
-            <div class="thumbnail">
-                <?php
-                if ( has_post_thumbnail() ) {
-                    the_post_thumbnail();
-                }
-                ?>
-            </div>
-        <h2> <?php echo get_the_title(); ?> </h2>
-        <p> 
-            <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small> 
-        </p>
-        <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
-
-        <?php the_content(); ?>
-        <?php comments_template(); ?> 
-        </div>
-
-        <?php endwhile; else : ?>
-        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-        <?php endif; ?>
-</article>
-
+<div class= "article pb-5"> 
+<?php the_content(); ?>
+</div>
+<?php comments_template(); ?> 
+<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+</article> 
+    </div>
 </section>
+
+<?php echo get_template_part('template-parts/content', 'more') ?>
+
+<?php endif; ?>
 
 <?php
 get_footer();
