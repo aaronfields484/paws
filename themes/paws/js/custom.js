9579a9f0da195358044751d6e517436c9873b7d6
aaronfields484 <aaronfields484@gmail.com>
2022-05-11 14:13:20 -0700

    feat: comments styling

--- themes/paws/js/custom.js
+++ themes/paws/js/custom.js
@@ -13,6 +13,10 @@
 
 	//Comments Area
 	$('.comments-area .submit').addClass('btn btn-outline-primary');
+	$('.comments-area #textarea').addClass('form-control');
+	$('.comments-area #textarea').addClass('form-control');
+	$('.comments-area input').addClass('form-control');
+	$('.comments-area label').addClass('form-label');
 	
 }( jQuery ) );
 
