dd1d045eb21ea44d359ec5e9218c5f1352aa090a
aaronfields484 <aaronfields484@gmail.com>
2022-05-11 11:12:39 -0700

    fix: missing code for about page

--- themes/paws/page.php
+++ themes/paws/page.php
@@ -2,9 +2,15 @@
 
 get_header();
 ?>
+<div class="container mt-5">
+<div class="about-page">
+    <?php if(has_post_thumbnail()){
+        the_post_thumbnail('pagethumbnail' );}?>
+    
+<?php the_content(  )?>   
+</div>
 
-	
+</div>
 
-<?php
 
-get_footer();
+<?php get_footer( )?>
